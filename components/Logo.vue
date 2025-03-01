cc20239f9ab4177945539a2fc88ad0c8dcaa7ce0
Mojahidul Islam <itsemon245@gmail.com>
2025-03-01 18:30:46 +0600

    fix: landing page

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,4 +1,18 @@
 <template>
-    <img width="150" class="dark:hidden inline-block" src="/logo.png"/>
+    <img width="150" class="dark:hidden inline-block" src="/logo.png" />
     <!-- <img width="120" class="dark:inline-block hidden" src="/logo-dark.png"/> -->
-</template>  
+</template>
+
+<style>
+@media (min-width: 1024px) {
+    .block-hero .layout {
+        gap: var(--space-8);
+        grid-template-columns: repeat(4, minmax(0, 1fr)) !important;
+    }
+
+    .block-hero .support {
+        grid-column-start: 3;
+        grid-column-end: 5;
+    }
+}
+</style>
