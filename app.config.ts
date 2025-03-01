a5c30a62dd58e665354645b607a268121b8a0580
Mojahidul Islam <itsemon245@gmail.com>
2025-03-01 15:30:55 +0600

    enabled index page

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Magic LMS',
+    description: 'The best learning management system',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       youtube: '@imjolofficial?si=n6Nx0_hOYvHbh3g5',
@@ -37,7 +37,7 @@ export default defineAppConfig({
       exclude: [],
       fluid: true
     },
-    footer:{
+    footer: {
       credits: false
     }
   }
